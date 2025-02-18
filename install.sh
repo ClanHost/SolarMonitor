df69e6037563cf45fca3d09a6c33b217c6dd40e0
ClanHost Australia <38305756+ClanHost@users.noreply.github.com>
2025-02-18 16:34:12 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,14 @@ sudo apt install mc -y
 sudo apt install htop -y
 sudo apt install python3-full -y
 sudo apt install python3-pip -y
+sudo apt install git -y
 sudo pip install mppsolar --break-system-packages
-
+sudo git clone https://github.com/bratwiz/jbdtool
+cd jbdtool
+# EDIT MAKE FILE AND SET 
+# DEBUG=yes
+# BLUETOOTH=no
+# MQTT=no
 
 
 sudo touch  /etc/systemd/system/SOLAR.service
